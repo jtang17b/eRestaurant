09484400e723d12646ac61368f5a6020bff08195
jtang17b <jtang17@studentmail.nait.ca>
2016-10-05 19:48:24 -0600

    eRestaurant entities

---
+++ eRestaurantSystem.Data/Entities/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace eRestaurantSystem.Data.Entities
+{
+    [Table("Item")]
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string Description { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public bool Active { get; set; }
+        public int? Calories { get; set; }
+        public string Comments { get; set; }
+        public int MenuCategoryID { get; set; }
+        public virtual MenuCategory MenCategory { get; set; }
+
+    }
+}
